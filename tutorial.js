e34f62702232ddb537fd5e772c81879eb588ac66
Oluwadamilola Oduntan <oduntandamilola@gmail.com>
2024-01-13 22:40:42 -0500

    Created React App

--- tutorial.js
+++ tutorial.js
@@ -1,3 +1,19 @@
 let name
 name = "Olu"
 console.log(`${name} is brilliant`)
+
+const strings = ["Home", "Shop", "About Me"];
+
+function capitalize(x) {
+    return x.toUpperCase()
+}
+
+const listItems = strings.map(capitalize);
+console.log(listItems)
+
+function keys(x, i) {
+  return `${x}_${i}`;
+}
+
+const listItemss = strings.map(keys);
+console.log(listItemss);
