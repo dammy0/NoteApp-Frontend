72e016ed7033e3a9f4c7d54896c740b5dd8f76c7
Oluwadamilola Oduntan <oduntandamilola@gmail.com>
2024-01-23 22:36:05 -0500

    Added Header and Note Components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import NotePage from './containers/NotePage';
 
 function App() {
   return (
     <div className="App">
-      My App
+      <NotePage />
     </div>
   );
 }
---
+++ src/containers/NotePage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from "../components/Header"
+import Notes from "../components/Notes"
+
+const NotePage = () => {
+  return (
+    <>
+      <div>NotePage</div>
+      <Header />
+      <Notes />
+    </>
+  );
+}
+
+export default NotePage
